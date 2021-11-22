fac3217f0e8c29d134f948421057912b461d2dc3
Student31CS <tyler31vickers@gmail.com>
2021-11-21 17:52:34 -0800

    Add Introduction

---
+++ text-adventure.cpp
@@ -0,0 +1,27 @@
+#include <iostream>
+#include <unistd.h>
+
+
+
+int main() {
+
+//Introduction to the story
+sleep(2); 
+std::cout << "Welcome.\n"; 
+sleep(2); 
+std::cout << "This is a game of choice, your choices will lead you down a path, and only your choices will end the story\n";
+sleep(2);
+std::cout << "....\n";
+sleep(2);
+std::cout << "You have just woken up in a cave\n";
+sleep(2);
+std::cout << "And you can't remember a thing\n";
+sleep(2);
+std::cout << "Next to you, there is a machete, a map, and a flashlight\n";
+
+
+// Declaring variables
+int choice1;
+int choice2;
+int choice3;
+}
